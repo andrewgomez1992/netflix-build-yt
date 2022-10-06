0cf9b0b4b78ff674c62b54350df4de5ffdb7a9b2
Andrew Gomez <andrewgomez@tx7lwr2gqx.tbc.limad.net>
2022-10-06 14:37:26 -0700

    tmdb api requests wip

--- src/Banner.js
+++ src/Banner.js
@@ -3,9 +3,9 @@ import './Banner.css'
 
 const Banner = () => {
 
-    // function truncate(string, n) {
-    //     return string?.length
-    // }
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string
+    }
 
     return (
         <header className='banner' style={{
@@ -20,7 +20,12 @@ const Banner = () => {
                     <button className="banner_button">My List</button>
                 </div>
                 <h1 className="banner_description">
-                    This is a test description
+                    {truncate(`This is a test descriptionThis is a test 
+                    description This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description`, 150)}
                 </h1>
             </div>
 
---
+++ src/Requests.js
@@ -0,0 +1,6 @@
+// just a demo, idgaf about hiding it :)
+const API_KEY = "6b499de003a32d7c424ae93f299daf0c"
+
+const requests = {
+
+}
