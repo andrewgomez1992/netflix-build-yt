56cea267f0d2c0e54d1b1010aec5ebf74c1ee507
Andrew Gomez <andrewgomez@tx7lwr2gqx.tbc.limad.net>
2022-10-05 20:59:57 -0700

    nav wip

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZWYZxSqiw8gjIx1JhN-Wr0kZjX3lb9HM",
+    authDomain: "netflix-build-yt-175cd.firebaseapp.com",
+    projectId: "netflix-build-yt-175cd",
+    storageBucket: "netflix-build-yt-175cd.appspot.com",
+    messagingSenderId: "7951023223",
+    appId: "1:7951023223:web:a3ff8cc703af736e619a72"
+  };
