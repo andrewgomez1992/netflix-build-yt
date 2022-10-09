4e5b16192d3d1aab5f3fd94ae0f6ba5f8709a1ce
Andrew Gomez <andrewgomez@tx7lwr2gqx.tbc.limad.net>
2022-10-09 11:44:41 -0700

    signOut button on profile page connected

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
@@ -23,13 +23,13 @@ const Nav = () => {
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
-        onClick={() => navigate('/')}
+          onClick={() => navigate("/")}
           className="nav_logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
         <img
-            onClick={() => navigate('/profile')}
+          onClick={() => navigate("/profile")}
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt=""
