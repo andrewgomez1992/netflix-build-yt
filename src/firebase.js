a689cb7f9afac30472e3ee681c2cee58d75c4014
Andrew Gomez <andrewgomez@tx7lwr2gqx.tbc.limad.net>
2022-10-09 10:55:08 -0700

    firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAZWYZxSqiw8gjIx1JhN-Wr0kZjX3lb9HM",
     authDomain: "netflix-build-yt-175cd.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "7951023223",
     appId: "1:7951023223:web:a3ff8cc703af736e619a72"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignupScreen from "./SignupScreen";
 
 const LoginScreen = () => {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
@@ -10,26 +13,34 @@ const LoginScreen = () => {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button className="loginScreen_button">Sign In</button>
+        <button onClick={() => setSignIn(true)} className="loginScreen_button">
+          Sign In
+        </button>
 
         <div className="loginScreen_gradient" />
 
         <div className="loginScreen_body">
-          <>
-            <h1>Unlimited films, TV programs, and more.</h1>
-            <h2>Watch anywhere. Cancel at any time.</h2>
-            <h3>
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </h3>
+          {signIn ? (
+            <SignupScreen />
+          ) : (
+            <>
+              <h1>Unlimited films, TV programs, and more.</h1>
+              <h2>Watch anywhere. Cancel at any time.</h2>
+              <h3>
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </h3>
 
-            <div className="loginScreen_input">
-              <form>
-                <input type="email" placeholder="Email Address" />
-                <button className="loginScreen_getStarted">GET STARTED</button>
-              </form>
-            </div>
-          </>
+              <div className="loginScreen_input">
+                <form>
+                  <input type="email" placeholder="Email Address" />
+                  <button className="loginScreen_getStarted">
+                    GET STARTED
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
