16ba288b7aef00595e631632d7b4eb3b941c64e8
Andrew Gomez <andrewgomez@tx7lwr2gqx.tbc.limad.net>
2022-10-09 13:55:55 -0700

    stripe data to firebase to app

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import Nav from "../Nav";
+import PlansScreen from "./PlansScreen";
 import "./ProfileScreen.css";
 
 const ProfileScreen = () => {
@@ -20,6 +21,8 @@ const ProfileScreen = () => {
           <div className="profileScreen_details">
             <h2>{user.email}</h2>
             <div className="profileScreen_plans">
+              <h3>Plans</h3>
+              <PlansScreen />
               <button
                 onClick={() => auth.signOut()}
                 className="profileScreen_signOut"
